44f2b330de39d6fcd90485325e031f31ec6e5de2
Lucas Azevedo <lucas.a.goes@aluno.senai.br>
2026-06-30 10:45:13 -0300

    Fazendo a divisão

---
+++ js/scripts_questao01.js
@@ -0,0 +1,21 @@
+//PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#div-geral')
+const divResultado = document.querySelector('#div-dados')
+
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let num1 = parseFloat(objFormDados.get('num1'))
+
+    // Verifica se é divisível por 3 e por 7
+if (num1 % 3 === 0 && num1 % 7 === 0) {
+    console.log(num1 + " é divisível por 3 e por 7.");
+} else {
+    console.log(num1 + " não é divisível por 3 e por 7.");
+}
+    
+
+})
